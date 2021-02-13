109d7059a5ac54dced2fc3b7c0b825b8247bfc74
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-02-13 21:10:11 +0100

    Updates

---
+++ precache-manifest.22af32e304ce65796ba6d033c6513e9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a383890fe60b2dd3d08a1fdb981bda8",
+    "url": "/selly-frontend/index.html"
+  },
+  {
+    "revision": "efe947faa5f7363a3379",
+    "url": "/selly-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7801c43b16bbb912ae62",
+    "url": "/selly-frontend/static/js/2.81fda503.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/selly-frontend/static/js/2.81fda503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe947faa5f7363a3379",
+    "url": "/selly-frontend/static/js/main.691e1487.chunk.js"
+  },
+  {
+    "revision": "2142b186ecd03fc36c5e",
+    "url": "/selly-frontend/static/js/runtime-main.798cb74e.js"
+  },
+  {
+    "revision": "f0f2d507f51aa7cbb819d6abd140a28d",
+    "url": "/selly-frontend/static/media/logo-01.f0f2d507.svg"
+  },
+  {
+    "revision": "702f89f18ee71be7256bd3e0aae00a33",
+    "url": "/selly-frontend/static/media/logo-03.702f89f1.png"
+  },
+  {
+    "revision": "445f3f75eadd12b245f649008f22c27b",
+    "url": "/selly-frontend/static/media/store.445f3f75.png"
+  }
+]);
